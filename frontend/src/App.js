145687cd42f633648f1ca4b0be3f231e81a5ebb4
Priyanka Kulkarni <pkulka2@uw.edu>
2025-07-10 16:09:58 -0700

    Intro screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+        <div className="bg-green-300 h-screen text-white flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Hello, world!</h1>
     </div>
+
   );
 }
 
---
+++ frontend/src/Components/Login-form.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+<div className="min-w-80 p-6 bg-Background-Default-Default rounded-lg outline outline-1 outline-offset-[-1px] outline-Border-Default-Default inline-flex flex-col justify-start items-start gap-6">
+  <div data-has-description="false" data-has-error="false" data-has-label="true" data-state="Default" data-value-type="Placeholder" className="self-stretch flex flex-col justify-start items-start gap-2">
+    <div className="self-stretch justify-start text-Text-Default-Default text-base font-normal font-['Inter'] leading-snug">Email</div>
+    <div className="self-stretch min-w-60 px-4 py-3 bg-Background-Default-Default rounded-lg outline outline-1 outline-offset-[-0.50px] outline-Border-Default-Default inline-flex justify-start items-center overflow-hidden">
+      <div className="flex-1 justify-start text-Text-Default-Tertiary text-base font-normal font-['Inter'] leading-none">Value</div>
+    </div>
+  </div>
+  <div data-has-description="false" data-has-error="false" data-has-label="true" data-state="Default" data-value-type="Placeholder" className="self-stretch flex flex-col justify-start items-start gap-2">
+    <div className="self-stretch justify-start text-Text-Default-Default text-base font-normal font-['Inter'] leading-snug">Password</div>
+    <div className="self-stretch min-w-60 px-4 py-3 bg-Background-Default-Default rounded-lg outline outline-1 outline-offset-[-0.50px] outline-Border-Default-Default inline-flex justify-start items-center overflow-hidden">
+      <div className="flex-1 justify-start text-Text-Default-Tertiary text-base font-normal font-['Inter'] leading-none">Value</div>
+    </div>
+  </div>
+  <div data-align="Justify" data-button-end="true" data-button-start="false" className="self-stretch inline-flex justify-start items-center gap-4">
+    <div data-has-icon-end="false" data-has-icon-start="false" data-size="Medium" data-state="Default" data-variant="Primary" className="flex-1 p-3 bg-Background-Brand-Default rounded-lg outline outline-1 outline-offset-[-1px] outline-Border-Brand-Default flex justify-center items-center gap-2 overflow-hidden">
+      <div className="justify-start text-Text-Brand-On-Brand text-base font-normal font-['Inter'] leading-none">Sign In</div>
+    </div>
+  </div>
+  <div className="self-stretch inline-flex justify-start items-start">
+    <div className="justify-start text-Text-Default-Default text-base font-normal font-['Inter'] underline leading-snug">Forgot password?</div>
+  </div>
+</div>
