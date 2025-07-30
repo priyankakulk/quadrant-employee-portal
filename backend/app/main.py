afe78308ca269e0df86dc0d409f96129d6aa1cd1
Rpuvvadi <rpuvvadi@vt.edu>
2025-07-30 01:46:43 -0400

    pushing induction

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,4 +17,5 @@ app.add_middleware(
 app.include_router(employees.router, prefix="/api")
 app.include_router(tickets.router, prefix="/api")
 app.include_router(leaveapplications.router, prefix="/api")  # ✅ ADD THIS LINE
+app.include_router(induction.router, prefix="/api")
 
